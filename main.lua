11534480456b1b52e6a4fa782913591ef2ac02de
Ali Mirza <tigerblaze09@gmail.com>
2023-12-02 00:05:23 +0500

    added apple summoning

--- main.lua
+++ main.lua
@@ -1,28 +1,30 @@
 function love.load()
+    local bump = require "bump"
     snekHead = love.graphics.newImage("snake.png")
     snekBody = love.graphics.newImage("snekBodyNew.png")
     currentHeadPositionX = 100
     currentHeadPositionY = 100
     score = 0;
-    
+    numOfApples = 0;
 end
 function love.update(dt)
     if love.keyboard.isDown("w") then
         currentHeadPositionY = currentHeadPositionY - 10
-    end
-
-    if love.keyboard.isDown("d") then
+    elseif love.keyboard.isDown("d") then
         currentHeadPositionX = currentHeadPositionX + 10
-    end
-    if love.keyboard.isDown("a") then
+    elseif love.keyboard.isDown("a") then
         currentHeadPositionX = currentHeadPositionX - 10
-    end
-    if love.keyboard.isDown("s") then
+    elseif love.keyboard.isDown("s") then
         currentHeadPositionY = currentHeadPositionY + 10
     end
-
+    if numOfApples == 0 then
+        appleCoordX = math.random(100, 950)
+        appleCoordY = math.random(50, 700)
+        numOfApples = numOfApples + 1
+    end
 end
 function love.draw()
-    love.graphics.draw(snekBody , currentHeadPositionX + 2, currentHeadPositionY + 32)
+    love.graphics.draw()
+    love.graphics.draw(snekBody, currentHeadPositionX + 2, currentHeadPositionY + 32)
     love.graphics.draw(snekHead, currentHeadPositionX, currentHeadPositionY)
 end
