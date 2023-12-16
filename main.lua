cd63310181feaea7c80c7e29c3de419ae206e163
Ali Mirza <tigerblaze09@gmail.com>
2023-12-16 22:48:37 +0500

    Update main.lua

--- main.lua
+++ main.lua
@@ -1,1 +1,17 @@
+function love.load()
+   snekDirection = "right"
+   snek = {} 
+end
 
+function love.update(dt)
+    if love.keyboard.isDown("w") and snekDirection ~= "down" then
+        snekDirection = "up"
+    elseif love.keyboard.isDown("s") and snekDirection ~= "up" then
+        snekDirection = "down"
+    elseif love.keyboard.isDown("a") and snekDirection ~= "right" then
+        snekDirection = "left"
+    elseif love.keyboard.isDown("d") and snekDirection ~= "left" then
+        snekDirection = "right"
+    end
+    print(snekDirection)
+end
