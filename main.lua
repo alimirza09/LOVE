83c7e38e2b6d3b75a30af01d9e32abaefe66b38f
Ali Mirza <tigerblaze09@gmail.com>
2023-12-31 23:58:09 +0500

    Better movement system!

--- main.lua
+++ main.lua
@@ -8,22 +8,16 @@ function love.load()
     turnpointY = 0
 end
 function love.update(dt)
-    if love.keyboard.isDown("w") and snekDirection ~= "down" then
-        snekDirection = "up"
-        turnpointX = snek.x
-        turnpointY = snek.y
-    elseif love.keyboard.isDown("s") and snekDirection ~= "up" then
-        snekDirection = "down"
-        turnpointX = snek.x
-        turnpointY = snek.y
-    elseif love.keyboard.isDown("a") and snekDirection ~= "right" then
-        snekDirection = "left"
-        turnpointX = snek.x
-        turnpointY = snek.y
-    elseif love.keyboard.isDown("d") and snekDirection ~= "left" then
-        snekDirection = "right"
-        turnpointX = snek.x
-        turnpointY = snek.y
+    function love.keypressed(scancode, isrepeat)
+        if scancode == "w" and snekDirection ~= "down" then
+            snekDirection = "up"
+        elseif scancode == "s" and snekDirection ~= "up" then
+            snekDirection = "down"
+        elseif scancode == "a" and snekDirection ~= "right" then
+            snekDirection = "left"
+        elseif scancode == "d" and snekDirection ~= "left" then
+            snekDirection = "right"
+        end
     end
     if snekDirection == "right" then
         snek.x = snek.x + 4
@@ -34,6 +28,14 @@ function love.update(dt)
     elseif snekDirection == "down" then
         snek.y = snek.y + 4
     end
+    if snekBodyX == turnpointX and snekBodyY == turnpointY and directionChanged then
+        directionChanged = false
+        snekBodyX = snek.x
+        snekBodyY = snek.y + 28
+    else
+        snekBodyX = snek.x
+        snekBodyY = snek.y
+    end
 end
 function love.draw()
     love.graphics.setColor(0.07451, 0.960784, 0.019608)
@@ -41,13 +43,13 @@ function love.draw()
     i = 1
     if snekDirection == "right" or snekDirection == "left" then
         while (i <= score + 3) do
-            love.graphics.rectangle("fill", snek.x + (28 * i), snek.y, 20, 20)
+            love.graphics.rectangle("fill", snekBodyX + (28 * i), snekBodyY, 20, 20)
             i = i + 1
         end
     end
     if snekDirection == "up" or snekDirection == "down" then
         while (i <= score + 3) do
-            love.graphics.rectangle("fill", snek.x, snek.y + (28 * i), 20, 20)
+            love.graphics.rectangle("fill", snekBodyX, snekBodyY + (28 * i), 20, 20)
             i = i + 1
         end
     end
